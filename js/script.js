30b18f0b77210edf725d1e389aba02d9594aaa2e
ramya unnikrishnan <ramyaunnikrishnan@ramyas-Air.attlocal.net>
2024-04-03 00:00:48 -0500

    add the changes

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
-// console.log("hi");
+
 $(document).ready(function () {
   $("#service-card-1").click(function () {
     $("#serviceModal1").modal("show");
@@ -46,41 +46,55 @@ document.querySelectorAll(".service-btn").forEach(function (button) {
   });
 });
 
-window.addEventListener("load", function () {
-  new Glider(document.querySelector(".glider"), {
-    testimonial_config: {
-      draggable: true,
-      slidesToScroll: 1,
-      slidesToShow: 1,
-      scrollLock: true,
-      dots: "#dots",
-      arrows: {
-        prev: ".glider-prev",
-        next: ".glider-next",
+var glider = new Glider(document.querySelector(".glider"), {
+  draggable: true,
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  scrollLock: true,
+  dots: "#dots",
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 2,
+        slidesToScroll: "auto",
+        itemWidth: 150,
+        duration: 0.25,
       },
-      responsive: [
-        {
-          // screens greater than >= 775px
-          breakpoint: 775,
-          settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 2,
-            slidesToScroll: "auto",
-            itemWidth: 150,
-            duration: 0.25,
-          },
-        },
-        {
-          // screens greater than >= 1024px
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            itemWidth: 150,
-            duration: 0.25,
-          },
-        },
-      ],
     },
-  });
+    {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+  ],
 });
+
+document
+  .querySelector(".glider")
+  .addEventListener("glider-loaded", function (event) {
+    glider.refresh(true);
+  });
+
+function scrollToSection(sectionId) {
+  var section = document.querySelector(sectionId);
+  console.log(section);
+  if (section) {
+    console.log(section.offsetTop);
+    console.log(document.querySelector("nav").offsetHeight);
+    // var offset = section.offsetTop - document.querySelector("nav").offsetHeight;
+    var offset = section.offsetTop;
+    window.scrollTo(0, offset);
+  }
+}
