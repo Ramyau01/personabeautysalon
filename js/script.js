b7e62d9afa19f22ae013a2c1ef67705588b469dd
ramya unnikrishnan <ramyaunnikrishnan@ramyas-Air.attlocal.net>
2024-04-04 15:24:26 -0500

    added preload and compressed images

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@
 
 // window.addEventListener("DOMContentLoaded", userScroll);
 
+// document.addEventListener("touchstart", onTouchStart, { passive: true });
+
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
