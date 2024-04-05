02e8febeddec1be86c8d2c7f8e625e15b4de7911
ramya unnikrishnan <ramyaunnikrishnan@ramyas-Air.attlocal.net>
2024-04-05 00:05:37 -0500

    fixed navbar and testimonial

--- js/script.js
+++ js/script.js
@@ -15,6 +15,24 @@
 // window.addEventListener("DOMContentLoaded", userScroll);
 
 // document.addEventListener("touchstart", onTouchStart, { passive: true });
+function toggleNavbar() {
+  var navbar = document.getElementById("navbarNav");
+  if (navbar.style.display === "flex") {
+    navbar.style.display = "none";
+  } else {
+    navbar.style.display = "flex";
+  }
+}
+document.getElementById("toggleButton").addEventListener("click", toggleNavbar);
+var navbarLinks = document.querySelectorAll("#navbarNav a");
+navbarLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    // Close the navbar when a link is clicked
+    var navbar = document.getElementById("navbarNav");
+    navbar.style.display = "none";
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
