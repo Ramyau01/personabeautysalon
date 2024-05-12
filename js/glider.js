7c38aa81cdf74faf45f0a21dde8efa3b0bb889ac
ramya unnikrishnan <ramyaunnikrishnan@ramyas-Air.attlocal.net>
2024-05-12 17:02:29 -0500

    changes in gallery page

--- js/glider.js
+++ js/glider.js
@@ -1,38 +1,79 @@
-window.addEventListener("load", function () {
-  new Glider(document.querySelector(".glider"), {
-    testimonial_config: {
-      draggable: true,
-      slidesToScroll: 1,
-      slidesToShow: 1,
-      scrollLock: true,
-      dots: "#dots",
-      arrows: {
-        prev: ".glider-prev",
-        next: ".glider-next",
+var glider = new Glider(document.querySelector(".glider"), {
+  draggable: true,
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  scrollLock: true,
+  dots: "#dots",
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 2,
+        slidesToScroll: "auto",
+        itemWidth: 150,
+        duration: 0.25,
       },
-      responsive: [
-        {
-          // screens greater than >= 775px
-          breakpoint: 775,
-          settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 2,
-            slidesToScroll: "auto",
-            itemWidth: 150,
-            duration: 0.25,
-          },
-        },
-        {
-          // screens greater than >= 1024px
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            itemWidth: 150,
-            duration: 0.25,
-          },
-        },
-      ],
     },
-  });
+    {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+  ],
 });
+
+document
+  .querySelector(".glider")
+  .addEventListener("glider-loaded", function (event) {
+    glider.refresh(true);
+  });
+
+// window.addEventListener("load", function () {
+//   new Glider(document.querySelector(".glider"), {
+//     testimonial_config: {
+//       draggable: true,
+//       slidesToScroll: 1,
+//       slidesToShow: 1,
+//       scrollLock: true,
+//       dots: "#dots",
+//       arrows: {
+//         prev: ".glider-prev",
+//         next: ".glider-next",
+//       },
+//       responsive: [
+//         {
+//           // screens greater than >= 775px
+//           breakpoint: 775,
+//           settings: {
+//             // Set to `auto` and provide item width to adjust to viewport
+//             slidesToShow: 2,
+//             slidesToScroll: "auto",
+//             itemWidth: 150,
+//             duration: 0.25,
+//           },
+//         },
+//         {
+//           // screens greater than >= 1024px
+//           breakpoint: 1024,
+//           settings: {
+//             slidesToShow: 4,
+//             slidesToScroll: 1,
+//             itemWidth: 150,
+//             duration: 0.25,
+//           },
+//         },
+//       ],
+//     },
+//   });
+// });
