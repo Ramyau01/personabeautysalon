53b9d77826b57acf9c320618c5ab46ef4276477d
ramya unnikrishnan <ramyaunnikrishnan@ramyas-Air.attlocal.net>
2024-05-15 15:51:02 -0500

    fixed gallery and testimonial

--- js/glider.js
+++ js/glider.js
@@ -11,10 +11,21 @@ var glider = new Glider(document.querySelector(".glider"), {
   responsive: [
     {
       // screens greater than >= 775px
-      breakpoint: 775,
+      breakpoint: 577,
       settings: {
         // Set to `auto` and provide item width to adjust to viewport
         slidesToShow: 2,
+        slidesToScroll: "auto",
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 3,
         slidesToScroll: "auto",
         itemWidth: 150,
         duration: 0.25,
